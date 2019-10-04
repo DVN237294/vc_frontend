97a2bfcfe513d9d4dd5c17c92f57f2ee7c341621
amavin <237294@via.dk>
2019-10-04 22:57:44 +0200

    Using api service component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//API
+import { ApiModule } from '../service/api.module'
+import { HttpClientModule } from '@angular/common/http'
+import { BASE_PATH } from '../service';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ApiModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ {provide: BASE_PATH, useValue: environment.API_BASE_PATH } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
