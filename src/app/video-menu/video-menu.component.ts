a77618e6660d4eb285eb59e4e14637e3b38294cf
sara <254272@via.dk>
2019-10-06 18:23:07 +0200

    added video menu component and boxes

---
+++ src/app/video-menu/video-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-menu',
+  templateUrl: './video-menu.component.html',
+  styleUrls: ['./video-menu.component.css']
+})
+export class VideoMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
