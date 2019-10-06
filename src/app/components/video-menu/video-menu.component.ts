ce308430caa759d5e5fb16fc3d0103e67f79c868
amavin <237294@via.dk>
2019-10-07 00:49:14 +0200

    video-tile to take video model class as input. Working on bindings

--- src/app/components/video-menu/video-menu.component.ts
+++ src/app/components/video-menu/video-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Video } from 'src/service';
 
 @Component({
   selector: 'app-video-menu',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoMenuComponent implements OnInit {
 
+  videos:Video[]
   constructor() { }
 
   ngOnInit() {
+    this.videos = [
+      {
+        id: 1,
+        url: "testing",
+        duration: 333,
+        name: "fdf1"
+      },
+      {
+        id: 2,
+        url: "testing2",
+        duration: 333,
+        name: "fdf2"
+      },
+      
+      {
+        id: 3,
+        url: "testing3",
+        duration: 333,
+        name: "fdf3"
+      }
+    ];
   }
 
 }
