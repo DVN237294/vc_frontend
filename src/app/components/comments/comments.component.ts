6d398a0656cc890e30ad639e07ef1980a1c0183a
Sara Nunes <saramcs16@gmail.com>
2019-10-25 19:52:42 +0200

    use comments api

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CommentsService, Comment } from 'src/api';
+import { environment } from '../../../environments/environment'
+
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.css']
 })
+
 export class CommentsComponent implements OnInit {
 
-  constructor() { }
+  @Input() comments: Comment[] = [];
+  
+  constructor(private commentsApi: CommentsService) {
+   }
 
   ngOnInit() {
+    if(this.comments.length === 0)
+      this.commentsApi.apiCommentsIdGet(environment.FRONTPAGE_VIDEO_LIMIT).subscribe(com => this.comments = com);  
   }
-
 }
