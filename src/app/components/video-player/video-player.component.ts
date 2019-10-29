12cb188dc1c29b88d13ca746665eddee52f19255
Sara Nunes <saramcs16@gmail.com>
2019-10-29 16:04:48 +0100

    removed unused imports. Bye forever

--- src/app/components/video-player/video-player.component.ts
+++ src/app/components/video-player/video-player.component.ts
@@ -1,24 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
-import { Comment, CommentsService, Video } from 'src/api';
+import { Video } from 'src/api';
 
 @Component({
   selector: 'app-video-player',
   templateUrl: './video-player.component.html',
   styleUrls: ['./video-player.component.css']
 })
-export class VideoPlayerComponent implements OnInit {   
+export class VideoPlayerComponent implements OnInit {
   video: Video;
-  
-  constructor(private commentsService: CommentsService) {
+
+  constructor() {
   }
 
   ngOnInit() {
     this.video = history.state;
   }
-  
-  }
+
+}
 
 
- 
+
 
