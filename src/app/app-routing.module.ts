beea5471fcbabb76144f47e37da2357e9751ae27
Sara Nunes <saramcs16@gmail.com>
2019-11-10 16:11:15 +0100

    created enrollments component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,12 @@ import { VideoMenuComponent } from './components/video-menu/video-menu.component
 import { VideoPlayerComponent } from './components/video-player/video-player.component';
 import { MyEnrollmentVideosComponent } from './components/my-enrollment-videos/my-enrollment-videos.component';
 import { AuthGuard } from './guards/authguard.guard';
-import { FrontpageGuard } from './guards/frontpage.guard';
 import { AboutComponent } from './components/about/about.component';
 import { CommentsComponent } from './components/comments/comments.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = [
-  { path: '', component: VideoMenuComponent, canActivate: [FrontpageGuard] },
+  { path: '', component: VideoMenuComponent},
   { path: 'video-menu', component: VideoMenuComponent },
   { path: 'myEnrollments', component: MyEnrollmentVideosComponent, canActivate: [AuthGuard] },
   { path: 'video', component: VideoPlayerComponent },
